1127bd16ae71d024779675b0949328531314a9e8
dd0sxx <contacttheotelonis@gmail.com>
2022-01-21 15:44:03 -0700

    got rid of metamask stuff for now

--- dapp/src/components/App.js
+++ dapp/src/components/App.js
@@ -15,39 +15,39 @@ function App() {
     const totalSupply = 100
 
     let provider
-    window.ethereum.enable().then(provider = new ethers.providers.Web3Provider(window.ethereum, "mainnet"))
-    const signer = provider.getSigner()
-    window.signer = signer
-    // const nftAddr = '0xDC04D8183a0C91c40E02dd5a0e06Ee6a2D25685F'
-    // const contract = new ethers.Contract(nftAddr, tigerNFTABI, provider);
-    const mainnet_chain_id = "0x1"
+    // window.ethereum.enable().then(provider = new ethers.providers.Web3Provider(window.ethereum, "mainnet"))
+    // const signer = provider.getSigner()
+    // window.signer = signer
+    // // const nftAddr = '0xDC04D8183a0C91c40E02dd5a0e06Ee6a2D25685F'
+    // // const contract = new ethers.Contract(nftAddr, tigerNFTABI, provider);
+    // const mainnet_chain_id = "0x1"
 
-    const sortOptions = ['by id', 'price low to high', 'price high to low', 'by tokens owned', 'last sold']
+    // const sortOptions = ['by id', 'price low to high', 'price high to low', 'by tokens owned', 'last sold']
 
-    window.ethereum.on('chainChanged', handleChainChanged);
+    // window.ethereum.on('chainChanged', handleChainChanged);
 
-    function handleChainChanged(_chainId) {
-        if (_chainId.toString() !== mainnet_chain_id) {
-            setChainWarning(true)
-        }
-        window.location.reload()
-    }
+    // function handleChainChanged(_chainId) {
+    //     if (_chainId.toString() !== mainnet_chain_id) {
+    //         setChainWarning(true)
+    //     }
+    //     window.location.reload()
+    // }
 
-    async function connectToMetamask() {
-        try {
-            let res = await signer.getAddress()
-            console.log("Signed in", res)
-            setAddress(res)
-            let _chainId = (await window.ethereum.request({ method: 'eth_chainId' }))
-            if (_chainId.toString() !== mainnet_chain_id) {
-                setChainWarning(true)
-            }
-        }
-        catch(err) {
-            console.log("Not signed in")
-            await provider.send("eth_requestAccounts", [])
-        }
-    }
+    // async function connectToMetamask() {
+    //     try {
+    //         let res = await signer.getAddress()
+    //         console.log("Signed in", res)
+    //         setAddress(res)
+    //         let _chainId = (await window.ethereum.request({ method: 'eth_chainId' }))
+    //         if (_chainId.toString() !== mainnet_chain_id) {
+    //             setChainWarning(true)
+    //         }
+    //     }
+    //     catch(err) {
+    //         console.log("Not signed in")
+    //         await provider.send("eth_requestAccounts", [])
+    //     }
+    // }
 
     // const getWalletOfOwner = async () => {
     //     console.log('meow')
@@ -63,7 +63,7 @@ function App() {
     //     return walletOfOwner
     // }
 
-    useEffect(() => {connectToMetamask().catch(err => console.error(err))}, [])
+    // useEffect(() => {connectToMetamask().catch(err => console.error(err))}, [])
 
     // useEffect(() => {
     //     if (address) getWalletOfOwner().catch(err => console.error(err))
@@ -71,7 +71,10 @@ function App() {
 
     return (
         <div className="app">
-        <Header address={address} connect={connectToMetamask}/>
+        <Header 
+        address={address} 
+        // connect={connectToMetamask}
+        />
     
             <ChainMsg open={chainWarning} setOpen={setChainWarning}/>
             {/* <BuyDialog currentlyBuying={currentlyBuying} setCurrentlyBuying={setCurrentlyBuying} contract={contract} address={address} signer={signer}/> */}
